2d87ccec5311e8bf4c10affc9e5bf274bcb974b1
Rokunuzzaman Bhuiya <rokunuzzamanbhuiya@gmail.com>
2021-10-31 21:40:55 +0600

    connected to client site

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const { MongoClient } = require("mongodb");
 require("dotenv").config();
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER}@cluster0.051fo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.051fo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 console.log(uri);
 
@@ -14,26 +19,55 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
 });
 
-// client.connect((err) => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
 
 async function run() {
   try {
     await client.connect();
-    console.log("database connected");
+    const database = client.db("online_services");
+    const servicesCollection = database.collection("services");
+
+    // GET API
+    app.get("/services", async (req, res) => {
+      const cursor = servicesCollection.find({});
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    // GET Single Service
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log("getting specific service", id);
+      const query = { _id: ObjectId(id) };
+      const service = await servicesCollection.findOne(query);
+      res.json(service);
+    });
+
+    // POST API
+    app.post("/services", async (req, res) => {
+      const service = req.body;
+      console.log("hit the post api", service);
+
+      const result = await servicesCollection.insertOne(service);
+      console.log(result);
+      res.json(result);
+    });
+
+    // DELETE API
+    app.delete("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await servicesCollection.deleteOne(query);
+      res.json(result);
+    });
   } finally {
-    // await client.close()
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
-
 app.get("/", (req, res) => {
-  //console.log(res);
-  res.send("Hello World!");
+  console.log(res);
+  res.send("Example app is running!");
 });
 
 app.listen(port, () => {
