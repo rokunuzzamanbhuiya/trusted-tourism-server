11f421a480b08756515fd02c7a9ca1552bd26ba4
Rokunuzzaman Bhuiya <rokunuzzamanbhuiya@gmail.com>
2021-10-31 14:42:45 +0600

    some update

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
 const express = require("express");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+
 const app = express();
 const port = process.env.PORT || 5000;
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER}@cluster0.051fo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+console.log(uri);
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// client.connect((err) => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("database connected");
+  } finally {
+    // await client.close()
+  }
+}
+run().catch(console.dir);
+
+
 app.get("/", (req, res) => {
+  //console.log(res);
   res.send("Hello World!");
 });
 
